4adbae74caf29065349171f61a3d68667b057220
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2021-05-27 21:13:00 -0400

    Initial Commit

---
+++ Assets/Aqua Jump/Game/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum State
+    {
+        Starting,
+        Executing,
+        Ended
+    }
+
+    [HideInInspector]
+    public Action<PlayerController> onDrag;
+
+    public float minDragDistance = 0.03f;
+
+    public State state { get; private set; }
+
+    public Vector2 startPos { get; private set; }
+    public Vector2 currentPos { get; private set; }
+    public Vector2 deltaPos => currentPos - startPos;
+    private int m_oldTouchCount = 0;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTouches();
+        UpdateMouse();
+    }
+
+    private void UpdateTouches()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(m_oldTouchCount == 0)
+            {
+                startPos = touch.position;
+            }
+
+            currentPos = touch.position;
+
+            if(deltaPos.sqrMagnitude > minDragDistance * minDragDistance)
+            {
+                onDrag?.Invoke(this);
+            }
+        }
+
+        m_oldTouchCount = Input.touchCount;
+
+    }
+
+    private void UpdateMouse()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+            state = State.Starting;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            if(state == State.Executing)
+            {
+                state = State.Ended;
+                onDrag?.Invoke(this);
+            }
+            state = State.Ended;
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            currentPos = Input.mousePosition;
+
+            if(deltaPos.sqrMagnitude > minDragDistance * minDragDistance)
+            {
+                onDrag?.Invoke(this);
+            }
+
+            state = State.Executing;
+        }
+
+    }
+}
---
+++ Assets/Aqua Jump/Game/Scripts/Managers/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Player m_aqua;
+
+    [SerializeField]
+    private PlayerController m_controller;
+
+    [SerializeField]
+    private Camera m_camera;
+
+    [SerializeField]
+    private float m_wrappingBoundsExtent = 1.3f;
+
+    private List<BasePlatform> m_platforms = new List<BasePlatform>();
+
+    public Vector2 m_debugStartPos;
+    public Vector2 m_debugCurrentPos;
+
+    private void OnEnable()
+    {
+        m_controller.onDrag += OnDrag;
+    }
+
+    private void OnDisable()
+    {
+        m_controller.onDrag -= OnDrag;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrag(PlayerController controller)
+    {
+        Debug.Log("Dragging");
+        m_debugStartPos = m_camera.ScreenToWorldPoint(controller.startPos);
+        m_debugCurrentPos = m_camera.ScreenToWorldPoint(controller.currentPos);
+
+        if(controller.state == PlayerController.State.Ended)
+        {
+            m_debugStartPos = Vector2.zero;
+            m_debugCurrentPos = Vector2.zero;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawLine(m_debugStartPos, m_debugCurrentPos);
+
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireCube(m_camera.transform.position, new Vector2(m_camera.orthographicSize * 2 * m_camera.aspect * m_wrappingBoundsExtent, m_camera.orthographicSize * 2));
+    }
+}
