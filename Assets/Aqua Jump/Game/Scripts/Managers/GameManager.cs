f8b8407d959b17beb2fb1d7ef00cb303fa9ecfb4
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2021-05-29 13:36:29 -0400

    - Modified: Player Player can now be launched at a given direction and power

--- Assets/Aqua Jump/Game/Scripts/Managers/GameManager.cs
+++ Assets/Aqua Jump/Game/Scripts/Managers/GameManager.cs
@@ -50,6 +50,7 @@ public class GameManager : MonoBehaviour
 
         if(controller.state == PlayerController.State.Ended)
         {
+            m_aqua.Launch((m_debugStartPos - m_debugCurrentPos).normalized, (m_debugStartPos - m_debugCurrentPos).magnitude);
             m_debugStartPos = Vector2.zero;
             m_debugCurrentPos = Vector2.zero;
         }
--- Assets/Aqua Jump/Game/Scripts/Player/Player.cs
+++ Assets/Aqua Jump/Game/Scripts/Player/Player.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D m_rigidBody;
+
+    public float powerMultiplier = 2.0f;
+    public float maxPower = 10.0f;
+
+
+    public Vector2 launchDirection { get; private set; }
+    public float launchPower { get; private set; }
+
+    private void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        m_rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +30,14 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Launch(Vector2 direction, float power)
+    {
+        launchDirection = direction;
+        launchPower = Mathf.Min(power * powerMultiplier, maxPower);
+
+        m_rigidBody.AddForce(launchDirection * launchPower, ForceMode2D.Impulse);
     }
 
     public PlayerCollisionVisitor CreateVisitor(GameManager gameManager)
